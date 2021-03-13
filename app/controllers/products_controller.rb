aa670b6e74258b440d06cd9ddd3e4c4a83bcdae5
yuurya <yuki_pooh1990@icloud.com>
2021-03-13 16:05:38 +0900

    詳細ページ完了

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,14 @@
 class ProductsController < ApplicationController
   def index
+    
     @products = Product.all
     @product = Product.new
+   
   end
 
   def show
     @product = Product.find(params[:id])
+    
   end
 
   def new
--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module UFO
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.time_zone = 'Asia/Tokyo'
   end
 end
